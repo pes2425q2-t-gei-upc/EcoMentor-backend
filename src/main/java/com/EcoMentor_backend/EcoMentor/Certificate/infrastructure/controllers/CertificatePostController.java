5021d0d9d44404c900d70b3123a9faa72f831f6f
victor.diez <04.diez.v@gmail.com>
2025-03-16 16:44:42 +0100

    Task#243 OfficialCertificatePost fixed

--- src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/infrastructure/controllers/CertificatePostController.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/infrastructure/controllers/CertificatePostController.java
@@ -2,6 +2,7 @@ package com.EcoMentor_backend.EcoMentor.Certificate.infrastructure.controllers;
 
 import com.EcoMentor_backend.EcoMentor.Certificate.useCases.CreateCertificateUseCase;
 import com.EcoMentor_backend.EcoMentor.Certificate.useCases.dto.CreateCertificateDTO;
+import com.EcoMentor_backend.EcoMentor.Certificate.useCases.dto.CreateOfficialCertificateDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -22,7 +23,7 @@ public class CertificatePostController {
     }
 
     @PostMapping("/official_certificate")
-    public ResponseEntity<Void> createOfficialCertificate(@RequestBody @Validated CreateCertificateDTO officialCertificate) {
+    public ResponseEntity<Void> createOfficialCertificate(@RequestBody @Validated CreateOfficialCertificateDTO officialCertificate) {
         createCertificateUseCase.execute(officialCertificate);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
