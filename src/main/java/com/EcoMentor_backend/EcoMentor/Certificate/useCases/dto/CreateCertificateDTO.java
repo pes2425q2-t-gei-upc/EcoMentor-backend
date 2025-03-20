b77ff803b36546149acdfcda35c8d9c701b978b4
David <david.sanz.martinez@estudiantat.upc.edu>
2025-03-21 00:31:26 +0100

    task#199 fixed inheritance in the DTOs of the certificates

--- src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/useCases/dto/CreateCertificateDTO.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/useCases/dto/CreateCertificateDTO.java
@@ -5,6 +5,9 @@ import com.EcoMentor_backend.EcoMentor.Address.useCases.dto.CreateAddressDTO;
 import com.EcoMentor_backend.EcoMentor.Certificate.entity.Certificate;
 import com.EcoMentor_backend.EcoMentor.Certificate.entity.CertificateType;
 import com.EcoMentor_backend.EcoMentor.Recommendation.entity.Recommendation;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +16,11 @@ import lombok.experimental.SuperBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "certificateType")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CreateOfficialCertificateDTO.class, name = "OFFICIAL"),
+})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
