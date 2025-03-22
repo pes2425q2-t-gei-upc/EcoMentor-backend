d5459c530f3a743fea935b1a2d29660634ca9aad
victor.diez <04.diez.v@gmail.com>
2025-03-22 17:31:36 +0100

    fix#255 fix bug

--- src/test/java/com/EcoMentor_backend/EcoMentor/ExternalService/GeocodificatorTest/useCases/dto/CoordinatesDTOTest.java
+++ src/test/java/com/EcoMentor_backend/EcoMentor/ExternalService/GeocodificatorTest/useCases/dto/CoordinatesDTOTest.java
@@ -3,8 +3,6 @@ package com.EcoMentor_backend.EcoMentor.ExternalService.GeocodificatorTest.useCa
 import com.EcoMentor_backend.EcoMentor.ExternalService.Geocodificator.useCases.dto.CoordinatesDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -12,45 +10,52 @@ public class CoordinatesDTOTest {
 
 private CoordinatesDTO coordinatesDTO;
 
-@BeforeEach
-void setUp() {
-    MockitoAnnotations.openMocks(this);
-    coordinatesDTO = new CoordinatesDTO();
-}
+    @BeforeEach
+    void setUp() {
+        coordinatesDTO = new CoordinatesDTO();
+    }
 
-@Test
-void settingAndGettingLongitude() {
-    coordinatesDTO.setLongitude(2.1734f);
-    assertEquals(2.1734f, coordinatesDTO.getLongitude());
-}
+    @Test
+    void settingAndGettingName() {
+        coordinatesDTO.setName("Barcelona");
+        assertEquals("Barcelona", coordinatesDTO.getName());
+    }
 
-@Test
-void settingAndGettingLatitude() {
-    coordinatesDTO.setLatitude(41.3851f);
-    assertEquals(41.3851f, coordinatesDTO.getLatitude());
-}
+    @Test
+    void settingAndGettingLongitude() {
+        coordinatesDTO.setLongitude(2.1734f);
+        assertEquals(2.1734f, coordinatesDTO.getLongitude());
+    }
 
-@Test
-void coordinatesDTOEquality() {
-    CoordinatesDTO coordinatesDTO1 = new CoordinatesDTO(2.1734f, 41.3851f);
-    CoordinatesDTO coordinatesDTO2 = new CoordinatesDTO(2.1734f, 41.3851f);
-    assertEquals(coordinatesDTO1, coordinatesDTO2);
-}
+    @Test
+    void settingAndGettingLatitude() {
+        coordinatesDTO.setLatitude(41.3851f);
+        assertEquals(41.3851f, coordinatesDTO.getLatitude());
+    }
 
-@Test
-void coordinatesDTONonEquality() {
-    CoordinatesDTO coordinatesDTO1 = new CoordinatesDTO(2.1734f, 41.3851f);
-    CoordinatesDTO coordinatesDTO2 = new CoordinatesDTO(2.1735f, 41.3852f);
-    assertNotEquals(coordinatesDTO1, coordinatesDTO2);
-}
+    @Test
+    void coordinatesDTOEquality() {
+        CoordinatesDTO coordinatesDTO1 = new CoordinatesDTO("Barcelona", 2.1734f, 41.3851f);
+        CoordinatesDTO coordinatesDTO2 = new CoordinatesDTO("Barcelona", 2.1734f, 41.3851f);
+        assertEquals(coordinatesDTO1, coordinatesDTO2);
+    }
 
-@Test
-void coordinatesDTOBuilder() {
-    CoordinatesDTO coordinatesDTO = CoordinatesDTO.builder()
-            .longitude(2.1734f)
-            .latitude(41.3851f)
-            .build();
-    assertEquals(2.1734f, coordinatesDTO.getLongitude());
-    assertEquals(41.3851f, coordinatesDTO.getLatitude());
-}
+    @Test
+    void coordinatesDTONonEquality() {
+        CoordinatesDTO coordinatesDTO1 = new CoordinatesDTO("Barcelona", 2.1734f, 41.3851f);
+        CoordinatesDTO coordinatesDTO2 = new CoordinatesDTO("Madrid", 2.1744f, 41.3861f);
+        assertNotEquals(coordinatesDTO1, coordinatesDTO2);
+    }
+
+    @Test
+    void coordinatesDTOBuilder() {
+        CoordinatesDTO coordinatesDTO = CoordinatesDTO.builder()
+                .name("Barcelona")
+                .longitude(2.1734f)
+                .latitude(41.3851f)
+                .build();
+        assertEquals("Barcelona", coordinatesDTO.getName());
+        assertEquals(2.1734f, coordinatesDTO.getLongitude());
+        assertEquals(41.3851f, coordinatesDTO.getLatitude());
+    }
 }
