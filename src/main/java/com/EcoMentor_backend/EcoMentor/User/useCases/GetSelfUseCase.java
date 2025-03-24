2793465e4f484c74a30c622d1f19bfec98b18975
Rubén Palà Vacas <rubenyadriaya@gmail.com>
2025-03-24 20:07:57 +0100

    task#92 - Fix to GetSelfUseCase, missing @Transactional.

--- src/main/java/com/EcoMentor_backend/EcoMentor/User/useCases/GetSelfUseCase.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/User/useCases/GetSelfUseCase.java
@@ -8,8 +8,10 @@ import com.EcoMentor_backend.EcoMentor.User.useCases.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class GetSelfUseCase {
 
