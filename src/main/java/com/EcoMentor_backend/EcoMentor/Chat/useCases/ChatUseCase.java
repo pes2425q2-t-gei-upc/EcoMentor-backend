f8736a382de5bf565002d5f8dbea1cadd47b9f53
victor.diez <04.diez.v@gmail.com>
2025-04-26 12:27:46 +0200

    Task#308 implemented a inappropiate language detector

--- src/main/java/com/EcoMentor_backend/EcoMentor/Chat/useCases/ChatUseCase.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/Chat/useCases/ChatUseCase.java
@@ -7,8 +7,14 @@ import com.EcoMentor_backend.EcoMentor.User.infrastructure.repositories.UserRepo
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,8 +32,23 @@ public class ChatUseCase {
         this.userRepository = userRepository;
     }
 
-    public ChatResponseDTO execute(String message, Long userId, String chatName, LocalDateTime now) {
+    public ChatResponseDTO execute(String message, Long userId, String chatName, LocalDateTime now1) {
         boolean suspicus = false;
+        LocalDateTime now = LocalDateTime.now(); //todo: cambiar por now1
+
+        if (inappropriateLanguageDetector(message)) {
+            Chat chat = Chat.builder()
+                    .userId(userId)
+                    .message("")
+                    .response("")
+                    .timestamp(now)
+                    .chatName(chatName)
+                    .isSuspicious(true)
+                    .build();
+
+            repo.save(chat);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Inappropriate language detected");
+        }
 
         if (!userRepository.existsById(userId)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
@@ -97,4 +118,28 @@ public class ChatUseCase {
                 .isSuspicious(suspicus)
                 .build();
     }
+
+    boolean inappropriateLanguageDetector(String sentence) {
+        if (sentence == null) return false;
+
+        // 1) Carga del fichero badwords.properties
+        ResourceBundle bundle = ResourceBundle.getBundle("badwords");
+
+        // 2) Reúne todas las palabras prohibidas en un Set (para evitar duplicados)
+        Set<String> badWords = new HashSet<>();
+        for (String key : bundle.keySet()) {
+            badWords.add(bundle.getString(key).toLowerCase());
+        }
+
+        String patternSrc = "\\b("
+              +  badWords.stream()
+                        .map(Pattern::quote)
+                        .collect(Collectors.joining("|"))
+                + ")\\b";
+
+        Pattern pattern = Pattern.compile(patternSrc, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(sentence);
+        return matcher.find();
+    }
+
 }
