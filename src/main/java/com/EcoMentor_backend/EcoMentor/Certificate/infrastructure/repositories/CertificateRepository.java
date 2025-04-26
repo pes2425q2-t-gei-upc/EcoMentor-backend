23dc0fd842b0f99a2878b8f25737e56d596faa9b
David <david.sanz.martinez@estudiantat.upc.edu>
2025-04-26 18:55:57 +0200

    Task#123 Calcular certificat almost done, it lacks the insulation and ventilation calculus and linter passed

--- src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/infrastructure/repositories/CertificateRepository.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/infrastructure/repositories/CertificateRepository.java
@@ -1,9 +1,8 @@
 package com.EcoMentor_backend.EcoMentor.Certificate.infrastructure.repositories;
 
 import com.EcoMentor_backend.EcoMentor.Certificate.entity.Certificate;
-import java.util.List;
-
 import com.EcoMentor_backend.EcoMentor.Certificate.entity.Qualification;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
