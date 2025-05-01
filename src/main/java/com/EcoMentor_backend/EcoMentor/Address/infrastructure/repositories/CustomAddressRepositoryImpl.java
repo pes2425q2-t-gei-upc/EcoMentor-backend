fca38b31ae9e4833a89c4e3d4ad15a6c5de55794
David <04.mas.d@gmail.com>
2025-05-01 15:01:52 +0000

    Issue #327 Refactor address retrieval methods to use TREAT for certificate type handling

--- src/main/java/com/EcoMentor_backend/EcoMentor/Address/infrastructure/repositories/CustomAddressRepositoryImpl.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/Address/infrastructure/repositories/CustomAddressRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.EcoMentor_backend.EcoMentor.Address.infrastructure.repositories;
 
 import com.EcoMentor_backend.EcoMentor.Address.entity.Address;
-import com.EcoMentor_backend.EcoMentor.Certificate.entity.CertificateType;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import java.util.ArrayList;
@@ -15,21 +14,23 @@ public class CustomAddressRepositoryImpl implements CustomAddressRepository {
         this.entityManager = entityManager;
     }
 
-
     @Override
-    public List<Address> findAddresByCertificateByParameter(String parameter, Object value, double minLatitude,
-                                                            double maxLatitude, double minLongitude,
-                                                            double maxLongitude) {
+    public List<Address> findAddresByCertificateByParameter(String parameter, Object value,
+                                                            double minLatitude, double maxLatitude,
+                                                            double minLongitude, double maxLongitude) {
 
-        CertificateType official = CertificateType.OFFICIAL;
-        String jpql = "SELECT a FROM  Address a"
-                + " JOIN a.certificates c"
-                + " WHERE   c.certificateType = :" + official + " AND c." + parameter + " = :value "
-                + "AND ST_Within(a.location, ST_MakeEnvelope(:minLongitude, :minLatitude, :maxLongitude,"
-                + " :maxLatitude, 4326))";
+        String jpql = "SELECT a FROM Address a "
+                + "JOIN a.certificates c "
+                + "WHERE TYPE(c) = :certificateClass "
+                + "AND TREAT(c AS com.EcoMentor_backend.EcoMentor.Certificate.entity.OfficialCertificate)."
+                + parameter + " = :value "
+                + "AND ST_Within(a.location, ST_MakeEnvelope(:minLongitude, "
+                + ":minLatitude, :maxLongitude, :maxLatitude, 4326))";
 
         Query query = entityManager.createQuery(jpql);
 
+        query.setParameter("certificateClass", com.EcoMentor_backend.EcoMentor
+                .Certificate.entity.OfficialCertificate.class);
         query.setParameter("value", value);
         query.setParameter("minLatitude", minLatitude);
         query.setParameter("maxLatitude", maxLatitude);
@@ -41,33 +42,32 @@ public class CustomAddressRepositoryImpl implements CustomAddressRepository {
 
     @Override
     public List<Address> findAddressByCertificateByParameters(Map<String, Object> parameters,
-                                                              double minLatitude,
-                                                              double maxLatitude,
-                                                              double minLongitude,
-                                                              double maxLongitude) {
-        StringBuilder jpql = new StringBuilder("SELECT a FROM Address a JOIN a.certificates c WHERE");
+                                                              double minLatitude, double maxLatitude,
+                                                              double minLongitude, double maxLongitude) {
 
-        CertificateType official = CertificateType.OFFICIAL;
-        // Añadir condiciones dinámicamente
+        StringBuilder jpql = new StringBuilder("SELECT a FROM Address a JOIN a.certificates c WHERE ");
         List<String> conditions = new ArrayList<>();
-        conditions.add("c.certificateType = :" + official + " AND");
+
+        conditions.add("TYPE(c) = :certificateClass");
+
         for (String key : parameters.keySet()) {
-            conditions.add("c." + key + " = :" + key);
+            conditions.add("TREAT(c AS com.EcoMentor_backend.EcoMentor."
+                    + "Certificate.entity.OfficialCertificate)." + key + " = :" + key);
         }
 
-        // Añadir condición geográfica
         jpql.append(String.join(" AND ", conditions));
-        jpql.append(" AND ST_Within(a.location, ST_MakeEnvelope(:minLongitude, :minLatitude, :maxLongitude,"
-                + " :maxLatitude, 4326))");
+        jpql.append(" AND ST_Within(a.location, ST_MakeEnvelope(:minLongitude, :minLatitude, "
+                + ":maxLongitude, :maxLatitude, 4326))");
 
         Query query = entityManager.createQuery(jpql.toString());
 
-        // Establecer parámetros dinámicos
+        query.setParameter("certificateClass", com.EcoMentor_backend.EcoMentor
+                .Certificate.entity.OfficialCertificate.class);
+
         for (Map.Entry<String, Object> entry : parameters.entrySet()) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
 
-        // Establecer parámetros de geolocalización
         query.setParameter("minLatitude", minLatitude);
         query.setParameter("maxLatitude", maxLatitude);
         query.setParameter("minLongitude", minLongitude);
@@ -75,6 +75,4 @@ public class CustomAddressRepositoryImpl implements CustomAddressRepository {
 
         return query.getResultList();
     }
-
-
 }
