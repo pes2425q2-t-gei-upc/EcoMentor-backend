c84672c809ad9174173c2ac0e04ee6925d449ac3
David <david.sanz.martinez@estudiantat.upc.edu>
2025-05-25 04:18:49 +0200

    Task #354 Done

--- src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/useCases/dto/CalculateUnofficialCertificateDTO.java
+++ src/main/java/com/EcoMentor_backend/EcoMentor/Certificate/useCases/dto/CalculateUnofficialCertificateDTO.java
@@ -3,6 +3,9 @@ package com.EcoMentor_backend.EcoMentor.Certificate.useCases.dto;
 
 import com.EcoMentor_backend.EcoMentor.Address.useCases.dto.CreateAddressDTO;
 import java.time.Year;
+
+import com.EcoMentor_backend.EcoMentor.Certificate.useCases.KindOfHeating;
+import com.EcoMentor_backend.EcoMentor.Certificate.useCases.KindOfRefrigeration;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,21 +24,20 @@ public class CalculateUnofficialCertificateDTO {
     private Year buildingYear;
     private String buildingUse;
     private float cadastreMeters;
-    private int nonRenewablePrimaryEnergyAprox;
-    private float finalEnergyConsumption;
     private float annualCost;
     private boolean electricVehicle;
     private boolean solarThermal;
     private boolean photovoltaicSolar;
-    private boolean biomass;
-    private boolean districtNet;
-    private boolean geothermal;
+    private KindOfHeating kindOfHeating;
+    private KindOfRefrigeration kindOfRefrigeration;
+    private KindOfHeating kindOfAcs;
     private int insulation;
     private int windowEfficiency;
-    private int heatingEmissionsAprox;
-    private int refrigerationEmissionsAprox;
-    private int acsEmissionsAprox;
-    private int lightingEmissionsAprox;
     private int residentialUseVentilation;
     private boolean energeticRehabilitation;
+
+    private float heatingConsumption;        //en KWh anuales
+    private float refrigerationConsumption;  //en KWh anuales
+    private float acsConsumption;            //en KWh anuales
+    private float lightingConsumption;       //en KWh anuales
 }
